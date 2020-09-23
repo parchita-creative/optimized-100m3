0b683871a07337ecb66b119ab03a96f4c9aa92df
Parchita Creative <71734639+parchita-creative@users.noreply.github.com>
2020-09-23 15:49:37 -0500

    add tag site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: NewBiz - v2.1.0
-* Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function($) {
   "use strict";
 
