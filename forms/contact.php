55498a863a33354a60122a057eff3492f2c11a3d
Parchita Creative <71734639+parchita-creative@users.noreply.github.com>
2020-09-24 14:16:06 -0500

    update mail

--- forms/contact.php
+++ forms/contact.php
@@ -1,39 +1,54 @@
 <?php
  
-  $receiving_email_address = 'andres@100m3.com';
  
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  // CONFIGURACION CORREO
+  $destinatario = "andres@100m3.com";
+  $destinatario_cc = "";
+  $destinatario_bcc = "";
+  $asunto  = "Formulario de 100m3"; 
+  $mensaje  = "";
+  $campos_obligatorios  = Array();
+  $first_name = "first"; 
+  $last_name  = "last"; 
+  $email  = "email";
+  $phone  = "phone";
+  $message  = "message";
+
+   // CONFIGURACION HTML
+  $enviado_bien = "Su formulario ha sido enviado correctamente";
+  $enviado_mal  = "ERROR: No se pudo enviar";
+
+  // RECOGER DATOS 
+  reset ($_POST);
+  $mensaje .= "<table border=\"1\">";
+  while (list ($clave, $valor) = each ($_POST)) {
+    $clave = htmlspecialchars($clave);
+    $valor = htmlspecialchars(trim($valor));
+    $mensaje .= "<tr><th>" . $clave . "</th><td>" . $valor . "</td></tr>";
   }
+  $mensaje .= "<tr><th>Fecha petición:</th><td>" . date("d/m/Y H:i:s") . "</td></tr>";
+  $mensaje .= "</table>";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // VARIABLES INTERNAS
+  $first = $_POST[$first_name];
+  $last = $_POST[$last_name];
+  $email = $_POST[$email];
+  $email = $_POST[$phone];
+  $email = $_POST[$message];
+
+  $cabeceras = "MIME-Version: 1.0\r\n"; //para el envío en formato HTML 
+  $cabeceras .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+  if ($correo != "") {
+   $cabeceras .= "From: " . $first . " <" . $email . ">\r\n"; // Dirección del remitente 
+   $cabeceras .= "Reply-To: " . $first . " <" . $email . ">\r\n"; // Dirección de respuesta
+  }
+  if ($destinatario_cc != "") { $cabeceras .= "Cc: " . $destinatario_cc . "\r\n"; }
+  if ($destinatario_bcc != "") { $cabeceras .= "Bcc: " . $destinatario_bcc . "\r\n"; }
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['first-name'];
-  $contact->from_email = $_POST['email'];
-
- 
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['first-name'], 'From');
-  $contact->add_message( $_POST['last-name'], 'LastName');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['phone'], 'Phone');
-
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  if (mail($destinatario, $asunto, $mensaje, $cabeceras)) {
+    echo $enviado_bien;
+  } else {
+    echo $enviado_mal;
+  }
 ?>
